f68fc571479f3aebabfb6ff38c34667a7d98e7e7
aathilducky <84506416+AATHILDUCKY@users.noreply.github.com>
2023-09-30 20:31:40 +0530

    Update script.js
    add more json  data

--- script.js
+++ script.js
@@ -1,36 +1,107 @@
 // Sample JSON data for the cheat sheet with header, notes, and description
-const mynotes = [{
-        "header": "Header 1",
-        "notes": "Note 1",
-        "description": "1 2 3Description 3"
+const mynotes = [
+    {
+        "header": "Phishing Attacks",
+        "notes": "Phishing is a type of cyberattack where attackers impersonate trusted entities to trick victims into revealing sensitive information.",
+        "description": "Phishing, Social Engineering, Email Security"
     },
     {
-        "header": "Header 2",
-        "notes": "Note 2",
-        "description": "1 2Description 1"
+        "header": "Firewalls",
+        "notes": "Firewalls are network security devices that filter incoming and outgoing traffic to protect a network from unauthorized access and threats.",
+        "description": "Firewall, Network Security, Packet Filtering"
     },
     {
-        "header": "Header 3",
-        "notes": "Note 3",
-        "description": "1 2 3 4Description 2"
+        "header": "Malware Types",
+        "notes": "Common types of malware include viruses, worms, Trojans, ransomware, and spyware, each with unique characteristics and purposes.",
+        "description": "Malware, Virus, Worm, Trojan, Ransomware, Spyware"
     },
     {
-        "header": "Header 1",
-        "notes": "Note 1",
-        "description": "Description 4"
+        "header": "Two-Factor Authentication (2FA)",
+        "notes": "2FA adds an extra layer of security by requiring users to provide two different authentication factors, such as a password and a fingerprint, to access an account.",
+        "description": "2FA, Authentication, Security"
     },
     {
-        "header": "Header 2",
-        "notes": "Note 2",
-        "description": "Description 5"
+        "header": "Zero-Day Vulnerabilities",
+        "notes": "Zero-day vulnerabilities are software vulnerabilities that are exploited by hackers before a patch or fix becomes available, posing a significant threat.",
+        "description": "Zero-Day, Vulnerabilities, Security Flaws"
     },
     {
-        "header": "Header 3",
-        "notes": "Note 3",
-        "description": "Description 6"
+        "header": "Cryptography",
+        "notes": "Cryptography is the practice of securing information by converting it into a code, making it unreadable without the proper decryption key.",
+        "description": "Cryptography, Encryption, Decryption"
+    },
+    {
+        "header": "Penetration Testing",
+        "notes": "Penetration testing is a proactive security measure where ethical hackers simulate cyberattacks to identify vulnerabilities in a system before malicious hackers do.",
+        "description": "Penetration Testing, Ethical Hacking, Security Assessment"
+    },
+    {
+        "header": "DDoS Attacks",
+        "notes": "Distributed Denial of Service (DDoS) attacks overwhelm a target server or network with a flood of traffic, causing service disruptions.",
+        "description": "DDoS, Cyberattacks, Traffic Overload"
+    },
+    {
+        "header": "Network Segmentation",
+        "notes": "Network segmentation involves dividing a network into smaller, isolated segments to limit lateral movement of attackers and improve security.",
+        "description": "Network Segmentation, Security Zones, Isolation"
+    },
+    {
+        "header": "Incident Response Plan",
+        "notes": "An incident response plan outlines the steps to take when a security incident occurs, helping organizations mitigate damage and recover quickly.",
+        "description": "Incident Response, Security Plan, Recovery"
+    },
+    {
+        "header": "Social Engineering",
+        "notes": "Social engineering attacks manipulate individuals into divulging confidential information through psychological manipulation and deception.",
+        "description": "Social Engineering, Manipulation, Deception"
+    },
+    {
+        "header": "Patch Management",
+        "notes": "Patch management is the process of keeping software and systems up to date with the latest security patches to address known vulnerabilities.",
+        "description": "Patch Management, Software Updates, Vulnerability Remediation"
+    },
+    {
+        "header": "Antivirus Software",
+        "notes": "Antivirus software detects and removes malicious software, such as viruses and malware, from a computer or network.",
+        "description": "Antivirus, Malware Protection, Security Software"
+    },
+    {
+        "header": "Encryption Algorithms",
+        "notes": "Encryption algorithms like AES and RSA are used to protect data by converting it into a secure, unreadable format that can only be decrypted with the correct key.",
+        "description": "Encryption, AES, RSA, Data Security"
+    },
+    {
+        "header": "Security Awareness Training",
+        "notes": "Security awareness training educates employees and users about cybersecurity best practices to reduce the risk of human errors and security breaches.",
+        "description": "Security Awareness, Training, Employee Education"
+    },
+    {
+        "header": "Blockchain Technology",
+        "notes": "Blockchain technology provides a secure and tamper-proof way to record transactions, making it valuable for securing digital assets and data.",
+        "description": "Blockchain, Cryptocurrency, Decentralization"
+    },
+    {
+        "header": "Multi-factor Authentication (MFA)",
+        "notes": "MFA adds an extra layer of security by requiring users to provide multiple forms of verification, such as a password and a one-time code from a mobile app.",
+        "description": "MFA, Authentication, Security Layers"
+    },
+    {
+        "header": "Security Incident Logging",
+        "notes": "Security incident logs record events and activities on a network, helping organizations monitor for suspicious behavior and investigate security incidents.",
+        "description": "Security Logging, Event Monitoring, Suspicious Activity"
+    },
+    {
+        "header": "Ransomware Prevention",
+        "notes": "Preventing ransomware attacks involves regular backups, user education, and security software to protect against file encryption and extortion.",
+        "description": "Ransomware, Prevention, Backup Strategies"
+    },
+    {
+        "header": "Endpoint Security",
+        "notes": "Endpoint security solutions protect individual devices, like computers and smartphones, from cyber threats and ensure they adhere to security policies.",
+        "description": "Endpoint Security, Device Protection, Security Policies"
     }
-    // Add more data here...
 ];
+
 
 // Function to split the search input into individual keywords
 function getKeywords() {
